be8fd449252dc65e60acf651394f291ad4e794c8
mertgulerx <108007620+mertgulerx@users.noreply.github.com>
2025-10-21 21:26:47 +0300

    chore: initialize backend project

---
+++ backend/src/main/java/com/paradoks/agileproject/AgileProjectApplication.java
@@ -0,0 +1,13 @@
+package com.paradoks.agileproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileProjectApplication.class, args);
+	}
+
+}
