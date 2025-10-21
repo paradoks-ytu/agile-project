472a0cfd0f3cab8015f135008757f790c4f05ce6
mertgulerx <108007620+mertgulerx@users.noreply.github.com>
2025-10-21 22:33:50 +0300

    fix: fix incorrect repository class-id pair

--- backend/src/main/java/com/paradoks/agileproject/repository/AnnouncementRepository.java
+++ backend/src/main/java/com/paradoks/agileproject/repository/AnnouncementRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AnnouncementRepository extends JpaRepository<Integer, Announcement> {
+public interface AnnouncementRepository extends JpaRepository<Announcement, Integer> {
 }
