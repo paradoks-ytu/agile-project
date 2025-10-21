c34d416583ec95e67e1b7b6beaed644ca0917b81
Ensar Gök <116498667+ensargx@users.noreply.github.com>
2025-10-21 22:04:34 +0300

    chore: Frontend Katmanlı Mimari Oluşturma
    SCRUM-14 announcmenst servisi ve type'leri hazırlandı

---
+++ frontend/src/service/announcementsService.ts
@@ -0,0 +1,14 @@
+import type { Announcement } from "@/types/announcementsTypes";
+import type { PagedResponse } from "@/types/types";
+import { API_URL, sizedParams } from "./service";
+
+
+export const getAnnouncements = async (
+  page: number = 0,
+  size: number = 10
+): Promise<PagedResponse<Announcement>> => {
+  const params = sizedParams(page, size);
+  
+  return await fetch(`${API_URL}/announcements${params.toString()}`)
+    .then((res) => res.json());
+};
