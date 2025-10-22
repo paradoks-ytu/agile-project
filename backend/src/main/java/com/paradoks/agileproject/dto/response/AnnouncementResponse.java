b7a0e543f6f40bcc033771c2006cb49b040dbda0
mertgulerx <108007620+mertgulerx@users.noreply.github.com>
2025-10-22 11:00:01 +0300

    refac: refactor Announcement Service

--- backend/src/main/java/com/paradoks/agileproject/dto/response/AnnouncementResponse.java
+++ backend/src/main/java/com/paradoks/agileproject/dto/response/AnnouncementResponse.java
@@ -1,8 +1,11 @@
 package com.paradoks.agileproject.dto.response;
 
 import com.paradoks.agileproject.model.announcement.AnnouncementSeverity;
+import lombok.Data;
+
 import java.time.Instant;
 
+@Data
 public class AnnouncementResponse {
     private Integer announcementId;
 
